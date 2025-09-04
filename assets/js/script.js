bd1e026a64482b579f50ce3d63efdcc4012ab9a5
pankaj9891 <117561193+pankaj9891@users.noreply.github.com>
2025-09-04 16:04:32 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+menuToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+  });
+}
+
 const backTopBtn = document.querySelector("[data-back-top-btn]")
 const header = document.querySelector("[data-header]")
 
